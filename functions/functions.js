dbf969d2c8efe87c0a91c69db8294833b911593c
Islam52 <islamhihiza11@gmail.com>
2022-01-04 18:38:04 +0600

    добавил поиск самого длинного

--- functions/functions.js
+++ functions/functions.js
@@ -5,3 +5,24 @@ function mul(n, m) {
 let result = mul(1, 2);
 alert( result ); 
 
+ 
+let x = [1, 2, 3, 4, 5];
+
+function matching(first, second) {
+    if (first > second)
+        return -1;
+}
+
+numerics = x.sort(matching);
+document.writeln(numerics);
+
+
+const user = { 
+    contry: 'Australia', 
+    name: 'Germany', 
+    longest: 'United States of America' 
+}
+
+const {longest} = user
+
+console.log(longest);
